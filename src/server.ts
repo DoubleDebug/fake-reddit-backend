e4ff72d8851722b11adfe6bb106a1c7ed79dd069
DoubleDebug <duledodicc@gmail.com>
2022-05-09 22:31:25 +0200

    Refactor: changed route naming convention

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cron from 'node-cron';
+import swaggerUi from 'swagger-ui-express';
 import { CONFIG } from './utils/misc/setupConfig.js';
 import { setupMiddleware } from './utils/middleware/setupMiddleware.js';
 import { isSignedIn } from './utils/middleware/isSignedIn.js';
@@ -15,7 +16,7 @@ import { registerUserWithEmail } from './endpoints/registerUserWithEmail.js';
 import { getUserEmailByUsername } from './endpoints/getUserEmailByUsername.js';
 import { deleteAccount } from './endpoints/deleteAccount.js';
 import { isAdmin } from './utils/middleware/isAdmin.js';
-import { banUser } from './endpoints/banUser.js';
+import { banAccount } from './endpoints/banAccount.js';
 import { deleteComment } from './endpoints/deleteComment.js';
 import { log } from './utils/misc/log.js';
 import { getUserPosts } from './endpoints/getUserPosts.js';
@@ -24,28 +25,34 @@ import { getSavedPosts } from './endpoints/getSavedPosts.js';
 import { submitSubreddit } from './endpoints/submitSubreddit.js';
 import { updateAccount } from './endpoints/updateAccount.js';
 import { deleteUserAvatars } from './utils/firestore/deleteUserAvatars.js';
+import { readFileSync } from 'fs';
 
 const app = express();
 setupMiddleware(app);
 
 // ROUTES
-app.get('/getFeed', getPosts);
-app.get('/getCustomFeed', isSignedIn, getPostsCustom);
-app.get('/getUserPosts', getUserPosts);
-app.get('/getUserComments', getUserComments);
-app.get('/getSavedPosts', isSignedIn, getSavedPosts);
-app.get('/getUserPhotoURL/:uid', getUserPhotoURL);
-app.get('/getUserEmailByUsername', getUserEmailByUsername);
-app.post('/registerUserWithProvider', registerUserWithProvider);
-app.post('/registerUserWithEmail', registerUserWithEmail);
-app.post('/submitPost', isSignedIn, submitPost);
-app.post('/submitSubreddit', isSignedIn, submitSubreddit);
-app.patch('/updateAccount', isSignedIn, updateAccount);
-app.delete('/deletePost', isSignedIn, deletePost);
-app.delete('/deleteComment', isSignedIn, deleteComment);
-app.delete('/deleteFile', isSignedIn, deleteFile);
-app.delete('/deleteAccount', isSignedIn, deleteAccount);
-app.delete('/banUser', isSignedIn, isAdmin, banUser);
+app.get('/posts', getPosts);
+app.get('/posts/custom', isSignedIn, getPostsCustom);
+app.get('/user/posts', getUserPosts);
+app.get('/user/comments', getUserComments);
+app.get('/user/saved-posts', isSignedIn, getSavedPosts);
+app.get('/user/avatar/:uid', getUserPhotoURL);
+app.get('/user/email', getUserEmailByUsername);
+app.post('/register/with-provider', registerUserWithProvider);
+app.post('/register/with-email', registerUserWithEmail);
+app.post('/posts', isSignedIn, submitPost);
+app.post('/subreddits', isSignedIn, submitSubreddit);
+app.patch('/user/account', isSignedIn, updateAccount);
+app.delete('/posts', isSignedIn, deletePost);
+app.delete('/comments', isSignedIn, deleteComment);
+app.delete('/files', isSignedIn, deleteFile);
+app.delete('/user/account', isSignedIn, deleteAccount);
+app.delete('/user/ban', isSignedIn, isAdmin, banAccount);
+
+// DOCS
+const apiDocument = readFileSync('./docs/OpenAPI.json', 'utf-8');
+const apiDocumentJson = JSON.parse(apiDocument);
+app.use('/', swaggerUi.serve, swaggerUi.setup(apiDocumentJson));
 
 // STARTING SERVER
 app.listen(CONFIG.PORT, CONFIG.HOSTNAME, () => {
